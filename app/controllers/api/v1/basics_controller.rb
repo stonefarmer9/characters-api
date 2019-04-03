51cf5cdca00aa71d8c05b41469939ce1b2543774
stonefarmer9 <g.p.levaillant@gmail.com>
2019-04-03 21:04:34 +0100

    Feat: Basic stats post endpoint working

--- app/controllers/api/v1/basics_controller.rb
+++ app/controllers/api/v1/basics_controller.rb
@@ -1,11 +1,14 @@
 class Api::V1::BasicsController < ApplicationController
+
+  skip_before_action :verify_authenticity_token
+
   before_action :set_basic, only: [:show, :edit, :update, :destroy]
 
   # GET /basics
 
   def index
     @basics = Basic.all
-
+    p @basics
     render json: @basics
   end
 
@@ -18,36 +21,37 @@ class Api::V1::BasicsController < ApplicationController
   # GET /basics/new
 
   # GET /basics/1/edit
-  
+
   def edit
   end
 
   # POST /basics
 
   def create
+    p 'got to create method'
     @basic = Basic.new(basic_params)
-
+    p '.new worked'
     if @basic.save
-
+      p '.save worked'
       render json: @basic, status: :created, location: api_v1_basic_url(@basic)
 
     else
-
-      redner json: @basic.errors, status: :unprocessable_entity
+      p "GOT TO ERROR #{@basic.errors}"
+      render json: @basic.errors, status: :unprocessable_entity
 
     end
   end
 
   # PATCH/PUT /basics/1
   def update
-    respond_to do |format|
-      if @basic.update(basic_params)
-        format.html { redirect_to @basic, notice: 'Basic was successfully updated.' }
-        format.json { render :show, status: :ok, location: @basic }
-      else
-        format.html { render :edit }
-        format.json { render json: @basic.errors, status: :unprocessable_entity }
-      end
+
+    if @basic.update(basic_params)
+
+      render json: @basic
+
+    else
+
+      render json: @basic.errors, status: :unprocessable_entity
     end
   end
 
@@ -55,10 +59,6 @@ class Api::V1::BasicsController < ApplicationController
   # DELETE /basics/1.json
   def destroy
     @basic.destroy
-    respond_to do |format|
-      format.html { redirect_to basics_url, notice: 'Basic was successfully destroyed.' }
-      format.json { head :no_content }
-    end
   end
 
   private
@@ -69,6 +69,7 @@ class Api::V1::BasicsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def basic_params
-      params.require(:basic).permit(:name, :age, :sex, :class, :race, :height)
+      p 'got to params fxn'
+      params.require(:basic).permit(:name, :age, :sex, :classs, :race, :height)
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-  resources :skills
-  resources :basics
+
+  namespace :api do
+    namespace :v1 do
+      resources :basics, :skills
+    end
+  end
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
